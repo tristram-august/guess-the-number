2bf68b722a1f3c1448e69c029d2d10b40fb5dcec
tristram-august <tristramt@gmail.com>
2024-07-10 21:26:03 -0400

    addressed loc bug

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import numpy as np
-
 import random
-
-
 
 num_max = 100
 
@@ -11,35 +8,35 @@ CSV_FILE = ('game_data.csv')
 
 def get_data():
     return pd.read_csv(CSV_FILE)
-
-def write_data(df):
-    pd.DataFrame.to_csv(df)
-       
-        
+            
 df = get_data()
 
 games_played = df.game_number.unique()
 last_game = games_played[-1]
 current_game = last_game + 1
-print(current_game)
-
+print(f'Game #{current_game}.')
 
 def guess(x):
     random_number = random.randint(1,x)
     guess = 0
 
     while guess != random_number:
+        new_row = []
         guess = int(input(f'Guess a number between 1 and {x}: '))
         if guess < random_number:
-            df.loc[len(df.index)] = [current_game, guess, False, False, True]
+            new_row = {'game_number':current_game, 'guess':guess, 'correct':False, 'too high':False, 'too low':True}
+            df.loc[len(df)] = new_row
             print('Sorry, guess again. Too low.')
         elif guess > random_number:
-            df.loc[len(df.index)] = [current_game, guess, False, True, False]
+            new_row = {'game_number':current_game, 'guess':guess, 'correct':False, 'too high':True, 'too low':False}
+            df.loc[len(df)] = new_row
             print('Sorry, guess again. Too high.')
 
-    df.loc[len(df.index)] = [current_game, guess, True, False, False]
+    new_row = {'game_number':current_game, 'guess':guess, 'correct':True, 'too high':False, 'too low':False}
+    df.loc[len(df)] = new_row
     print(f'Congratulations! You have guessed the number {random_number} correctly!')
-    df.to_csv ('game_data.csv', mode='a', index=False, header=False)
+    df.to_csv(CSV_FILE, index=False)
+    
     
     
 guess(num_max)
